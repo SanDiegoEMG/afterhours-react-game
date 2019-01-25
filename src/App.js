9d2b17517df33276cea5684f00c127cb2145236c
SD_emg <sandiegoemg@gmail.com>
2019-01-24 21:44:25 -0800

    rendering photos from json onto screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import cards from "./cards_data.json";
 
 class App extends Component {
+  // anything that is in the state, is something that can be dynamically changed through user interaction
+  state = {
+    score: 0,
+    tally: 0,
+    bunnies: cards
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    <div> 
+      <div className="container">
+      <header>
+        <div className="row">
+          <div className="col-sm-4">
+            <h4> React 'til the end!</h4>
+          </div>
+        <div className="col-sm-4">
+          <h4>Or get outta the way!</h4>
+        </div>
+        <div className="col-sm-4">
+          <h2> Score: {this.state.score} | Tally: {this.state.tally} </h2>
+        </div>
       </div>
+      </header>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          {this.state.bunnies.map(bunny => (
+            <div 
+              key={bunny.id}
+              className="bun-card col-md-4" 
+              style={{backgroundImage: `url(${bunny.image})`}}>
+              <p>{bunny.name}</p>
+            </div>
+           ))}
+        </div>
+
+        <div className="row">  </div>
+
+    </div>
+    </div>
+
     );
   }
 }
